1e813b3edf06b06e449710ff111b58f60dc012dc
Linlin CAI <2462127222@qq.com>
2026-05-10 20:13:50 +0800

    deploy site to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  // If your repo is username.github.io, keep this as your domain.
-  // If your repo is personal-site, change site and add: base: '/personal-site'
-  site: 'https://your-github-username.github.io'
+  site: 'https://linlinCa.github.io',
+  base: '/linlincai',
 });
